f58515718c6dd9a389aae56971ee1117e8c34f62
Yury Krasavin <buggins@live.com>
2020-01-26 23:40:59 +0300

    SQUAD-9-313 Add Change button to the clients list

--- src/app/allclients/allclients.component.ts
+++ src/app/allclients/allclients.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 import Client from 'src/app/client';
 
 @Component({
@@ -10,16 +12,18 @@ import Client from 'src/app/client';
 export class AllclientsComponent implements OnInit {
   clientData: Client[];
 
-  constructor(private http: HttpClient) { 
-  }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.getList();
   }
-  async getList() {
-    return await this.http.get('api/v1/clients').subscribe((data: Client[]) => this.clientData = [ ...data]);
+  getList() {
+    return this.http.get('api/v1/clients').subscribe((data: Client[]) => this.clientData = [...data]);
   }
-  async deleteById(clientId) {
-    return await this.http.delete('api/v1/clients/' + clientId).subscribe(() => this.getList());
+  startUpdate(clientId: string) {
+    return this.router.navigate(['/updateform', clientId]);
+  }
+  deleteById(clientId: string) {
+    return this.http.delete('api/v1/clients/' + clientId).subscribe(() => this.getList());
   }
 }
